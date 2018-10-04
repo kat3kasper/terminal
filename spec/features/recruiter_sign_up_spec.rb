3ce9d8b6ac552a04f9d5691822b19dd96e26490b
Dave Loftis <dave@lofdev.com>
2018-10-04 09:48:41 -0600

    [#196 WIP]
    Preparing to add HABTM benefits to company.

--- spec/features/recruiter_sign_up_spec.rb
+++ spec/features/recruiter_sign_up_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 feature 'Recruiter sign up' do
   let(:recruiter_country) { 'US' }
   let!(:cultures) do
-      [
-        create(:culture, value: 'This is a culture.'),
-        create(:culture, value: 'This is also a culture.')
-      ]
+    [
+      create(:culture, value: 'This is a culture.'),
+      create(:culture, value: 'This is also a culture.')
+    ]
+  end
+  let!(:benefits) do
+    [
+      create(:benefit, value: 'This is a benefit.'),
+      create(:benefit, value: 'This is another benefit.')
+    ]
   end
   before do
-
     stub_request(:get, /ipinfo.io/).
       with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
       to_return(status: 200, body: recruiter_country, headers: {})
@@ -33,9 +38,11 @@ feature 'Recruiter sign up' do
     fill_in 'Industry', with: 'General Manufacturing'
     fill_in 'Url', with: 'http://example.com/'
     cultures.each { |c| check c.value }
+    benefits.each { |b| check b.value }
     click_on 'Confirm'
 
     expect(Company.last.cultures.length).to eq(cultures.length)
+    expect(Company.last.benefits.length).to eq(benefits.length)
 
     open_email('colin@example.com')
     current_email.click_link 'CLICK HERE'
