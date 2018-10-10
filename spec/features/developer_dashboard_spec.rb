4e441c776b17aed2bc30815ef46d0bb012c48b64
Rebecca <rfjackson11@gmail.com>
2018-10-10 15:53:10 -0600

    WIP

--- spec/features/developer_dashboard_spec.rb
+++ spec/features/developer_dashboard_spec.rb
@@ -6,10 +6,14 @@ feature 'Developer dashboard' do
   let(:active_company) { create :company, vetted: true }
   let!(:active_job) { create :job, :remote, company_id: active_company.id }
 
-  let(:benefit) { create :benefit, value: 'not my benefit' }
+  let(:benefit) { create :benefit, value: 'other benefit' }
   let(:culture) { create :culture, value: 'not my culture' }
   let(:other_company) { create :company, vetted: true, benefits: [benefit], cultures: [culture] }
   let!(:other_job) { create :job, :remote, company_id: other_company.id }
+
+  let(:all_benefits) { Benefit.all }
+  let(:company_with_all_benefits) { create :company, vetted: true, benefits: all_benefits }
+  let!(:job_with_all_benefits) { create :job, :remote, company_id: company_with_all_benefits.id }
 
   let(:inactive_company) { create :company }
   let!(:inactive_job) { create :job, :remote, company_id: inactive_company.id }
@@ -26,6 +30,17 @@ feature 'Developer dashboard' do
     check active_company.benefits.first.value
     expect(page).to_not have_content other_job.title.upcase
     expect(page).to have_content active_job.title.upcase
+    expect(page).to_not have_content inactive_job.title
+  end
+
+  scenario 'can filter by multiple benefits', js: true, focus: true do
+    click_on 'Benefits'
+    check active_company.benefits.first.value
+    check other_company.benefits.first.value
+    expect(page).to have_content job_with_all_benefits.title.upcase
+    expect(page).to_not have_content other_job.title.upcase
+    expect(page).to_not have_content active_job.title.upcase
+    expect(page).to_not have_content inactive_job.title
   end
 
   scenario 'can filter by cultures', js: true do
@@ -33,5 +48,6 @@ feature 'Developer dashboard' do
     check active_company.cultures.first.value
     expect(page).to_not have_content other_job.title.upcase
     expect(page).to have_content active_job.title.upcase
+    expect(page).to_not have_content inactive_job.title
   end
 end
